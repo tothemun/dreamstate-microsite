e8e0ba5a2562ac76f158078aad6c80c4906b00e6
Jonathan <jonathanbblair@gmail.com>
2017-11-13 15:32:24 -0500

    Removed fallingBlock class

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,8 +2,7 @@ var canvas = document.getElementById('canvas')
   , ctx = canvas.getContext('2d')
   , center = {x: canvas.width / 2, y: canvas.height / 2};
 
-var blocks = []
-  , gridBlocks = [];
+var gridBlocks = [];
 
 canvas.width = window. innerWidth - 20;
 canvas.height = window.innerHeight - 20;
@@ -19,7 +18,6 @@ window.onload = () => {
     drawGrid(100);
   };
 
-  drawBlocks(25);
   drawGrid(100);
   loop();
 }
@@ -30,12 +28,8 @@ function loop() {
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  for (var i = 1; i < blocks.length; i++){
-    blocks[i].update();
-  }
-
   for(var i = i; i < gridBlocks.length; i++) {
-    gridBlocks[i].draw();
+    gridBlocks[i].update();
   }
 }
 
@@ -44,6 +38,9 @@ function Block(x, y, width, height) {
   this.y = y;
   this.width = width;
   this.height = height;
+  this.dy = 0;
+  this.extrusion = 0;
+  this.extrusionDepth = 10;
 
   this.draw = function() {
     ctx.save();
@@ -52,57 +49,34 @@ function Block(x, y, width, height) {
     ctx.strokeStyle = 'white';
     ctx.strokeRect(this.x, this.y, this.height, this.width);
     ctx.stroke();
+
+    if(this.extrusion != 0) {
+      // Top Side
+      ctx.beginPath();
+      ctx.moveTo(this.x, this.y);
+      ctx.lineTo(this.x - this.extrusion, this.y - this.extrusion);
+      ctx.lineTo(this.x2 - this.extrusion, this.y - this.extrusion);
+      ctx.lineTo(this.x + this.blockWidth, this.y);
+      ctx.closePath();
+      ctx.fill();
+      ctx.stroke();
+
+      // Left Side
+      ctx.beginPath();
+      ctx.moveTo(this.x, this.y);
+      ctx.lineTo(this.x - this.extrusion, this.y  - this.extrusion);
+      ctx.lineTo(this.x - this.extrusion, this.y2 - this.extrusion);
+      ctx.lineTo(this.x, this.y + blockHeight);
+      ctx.closePath();
+      ctx.fill();
+      ctx.stroke();
+    }
+
     ctx.restore();
   }
-}
 
-function FallingBlock(x, y, dx, blockWidth, blockHeight) {
-  this.x = x;
-  this.y = y;
-  this.x2 = x + blockHeight;
-  this.y2 = y + blockWidth;
-  this.dx = dx;
-  this.blockWidth = blockWidth;
-  this.blockHeight = blockHeight;
-  this.mod = 10;
-
-  this.draw = function() {
-    ctx.save();
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = '#fb6aab';
-    ctx.fillStyle = 'black';
-
-
-
-
-    // Top Side
-    ctx.beginPath();
-    ctx.moveTo(this.x, this.y);
-    ctx.lineTo(this.x - this.mod, this.y - this.mod);
-    ctx.lineTo(this.x2 - this.mod, this.y - this.mod);
-    ctx.lineTo(this.x + this.blockWidth, this.y);
-    ctx.closePath();
-    ctx.fill();
-    ctx.stroke();
-
-    // Left Side
-    ctx.beginPath();
-    ctx.moveTo(this.x, this.y);
-    ctx.lineTo(this.x - this.mod, this.y  - this.mod);
-    ctx.lineTo(this.x - this.mod, this.y2 - this.mod);
-    ctx.lineTo(this.x, this.y + blockHeight);
-    ctx.closePath();
-    ctx.fill();
-    ctx.stroke();
-
-    // Front Side
-    ctx.beginPath();
-    ctx.fillRect(this.x, this.y, this.blockWidth, this.blockWidth);
-    ctx.strokeRect(this.x, this.y, this.blockWidth, this.blockWidth);
-    ctx.stroke();
-    ctx.closePath();
-
-    ctx.restore();
+  this.extrude = function() {
+    this.extrusion = 1;
   }
 
   this.update = function(){
@@ -110,21 +84,11 @@ function FallingBlock(x, y, dx, blockWidth, blockHeight) {
       this.y = roundTo((Math.random() * canvas.height), 45);
     }
 
-    this.y += this.dx;
+    this.y += this.dy;
     this.x2 = this.x + blockHeight;
     this.y2 = this.y + blockWidth;
 
     this.draw();
-  }
-}
-
-function drawBlocks(a) {
-  for (var i = 0; i <= a; ++i) {
-    var x = roundTo(Math.floor(Math.random() * canvas.width), 40);
-    var y = roundTo(Math.floor(Math.random() * canvas.height), 40);
-    var dx = (Math.random() + 0.2 ) * 1;
-
-    blocks.push(new FallingBlock(x, y, dx, 40, 40));
   }
 }
 
